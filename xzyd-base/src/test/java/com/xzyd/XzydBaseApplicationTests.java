b210ef9f2dae307f49a951640bca752b7bd14276
dll <dll0036@shu.edu.cn>
2023-03-08 23:31:03 +0800

    parent and base

---
+++ xzyd-base/src/test/java/com/xzyd/XzydBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XzydBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
